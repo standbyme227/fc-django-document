b93a30dfa8ed404b13e3419d3ae89b2704df047b
ShinJongMin <standbyme227@gmail.com>
2018-02-05 17:04:34 +0900

    기본 중개모델 사용법 예제추가

--- django/many_to_many/models.py
+++ django/many_to_many/models.py
@@ -20,6 +20,11 @@ class Pizza(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    like_users = models.ManyToManyField(
+        'User',
+        through='PostLike',
+        related_name = 'like_post',
+    )
 
     def __str__(self):
         return self.title
@@ -37,5 +42,12 @@ class Postlike(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(selfs):
-        return f'{post.title} {user.name} {created_date}'
+    def __str__(self):
+        # return f'"{self.post.title}"글의 좋아요({self.user.name}, {self.created_date})'
+        return f'"{title}"글의 좋아요({name}, {date})'.format(
+            title=self.post.title,
+            name=self.user.name,
+            date=datetime.strftime(
+                timezone.localtimee(self.created_date),
+                '%Y.%m.%d')
+        )
