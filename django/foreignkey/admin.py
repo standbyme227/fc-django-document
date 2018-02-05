6425dc6e44873fe28a7a4465d6e3fb0d721b9072
ShinJongMin <standbyme227@gmail.com>
2018-02-05 15:14:32 +0900

    foreignkey field 에서 primary_key를 따로 지정한 경우 실습

--- django/foreignkey/admin.py
+++ django/foreignkey/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Manufacturer, Car
+from .models import Manufacturer, Car, Person, Type, Pokemon
 
 admin.site.register(Manufacturer)
 admin.site.register(Car)
+admin.site.register(Person)
+admin.site.register(Type)
+admin.site.register(Pokemon)
--- django/foreignkey/models.py
+++ django/foreignkey/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
-class Manufacturer(models.Model):
-    name = models.CharField('제조사명',max_length=50)
 
-    def __str__(self):
-        return self.name
 
 
 class Car(models.Model):
     manufacturer = models.ForeignKey(
-        Manufacturer,
+        'Manufacturer', #문자열로 처리전에는 Manufacturer클래스가 아래에 있어서 참조를 못했었다.
         on_delete=models.CASCADE,
         verbose_name='제조사'
     )
@@ -18,3 +14,37 @@ class Car(models.Model):
     def __str__(self):
         return(f'{self.manufacturer}{self.name}')
         pass
+
+
+class Manufacturer(models.Model):
+    name = models.CharField('제조사명',max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+    teacher = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL, #만약에 CASCADE를 한다면 지정된 사람에 대해서 같이 삭제된다.
+        null=True,
+        blank=True,
+    )
+
+
+
+
+class Type(models.Model):
+    type_number = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f'{self.type_number}| {self.name}'
+
+class Pokemon(models.Model):
+    dex_number = models.IntegerField(primary_key=True)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.dex_number:03}. {self.name} {self.type.name}'
