8aac249543115d09f0a348fb2852b52bffe68f50
ShinJongMin <standbyme227@gmail.com>
2018-02-06 14:44:56 +0900

    follow, block, following, block_users구분

--- django/many_to_many/models/symmetrical_intermediate.py
+++ django/many_to_many/models/symmetrical_intermediate.py
@@ -17,10 +17,12 @@ class TwitterUser(models.Model):
         through='Relation',
         related_name='+',
     )
+
+
     def __str__(self):
         return self.name
 
-
+    @property
     def following(self):
         # 내가 from_user이며 , type이 팔로잉인 Relation의 쿼리
         following_relations = self.relations_by_from_user.filter(
@@ -33,6 +35,34 @@ class TwitterUser(models.Model):
 
         following_users = TwitterUser.objects.filter(pk__in=following_pk_list)
         return following_users
+
+    @property
+    def block_users(self):
+        block_relations = self.relations_by_from_user.filter(
+            type=Relation.RELATION_TYPE_BLOCK,
+        )
+        block_pk_list = block_relations.values_list('to_user', flat=True)
+        block_users = TwitterUser.objects.filter(pk__in=block_pk_list)
+        return block_users
+
+
+        # self.relations_by_from_user.filter(type=Relation.RELATION_TYPE_BLOCK).values_list('to_user', flat=True)
+        # return TwitterUser.objects.filter(pk__in=pk_list)
+
+
+    def follow(self, to_user):
+        self.relations_by_from_user.filter(to_user=to_user).delete()
+        self.relations_by_from_user.create(
+            to_user=to_user,
+            type=Relation.RELATION_TYPE_FOLLOWING,
+        )
+
+    def block(self, to_user):
+        self.relations_by_from_user.filter(to_user=to_user).delete()
+        self.relations_by_from_user.create(
+            to_user=to_user,
+            type=Relation.RELATION_TYPE_BLOCK,
+        )
 
 class Relation(models.Model):
     '''
@@ -62,4 +92,13 @@ class Relation(models.Model):
     #related_name을 설정한 이유는 각각의 reverse의 네임이 하나의 클래스를 통해서 구성되기에 충돌이 일어나기때문이다.
     #from_user의 반대는 Relation_Set이고 to_user도 마찬가지이다.
     type = models.CharField(max_length=1, choices=CHOICES_TYPE)
+    created_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = (
+            # from_user와 to_user의 값이 이미있을 경우
+            # DB에 중복 데이터 저장을 막음
+            # ex) from_user가 1, to_user가 3인 데이터가 이미 있다면
+            #   두 항복의 값이 모두 같은 또 다른 데이터가 존재할 수 없음
+            ('from_user', 'to_user'),
+        )
