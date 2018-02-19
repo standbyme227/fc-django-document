10aa6fba42e2a0ab20323b28ff34d962a9b3134c
ShinJongMin <standbyme227@gmail.com>
2018-02-19 11:51:28 +0900

    weblog app 추가, model 구현

--- app/config/settings.py
+++ app/config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'inheritance.proxy_manager.apps.ProxyManagerConfig',
     'inheritance.multiple_inheritance.apps.MultipleInheritanceConfig',
 
+    'weblog.apps.WeblogConfig',
+
     'django_extensions',
 
     'django.contrib.admin',
